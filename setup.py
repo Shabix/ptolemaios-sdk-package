19825e43b413a4f39577b2597dc2382f98dfc5fe
Shabix <kmharouna@gmail.com>
2020-09-12 11:57:00 +0300

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ptolemaios',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='git@github.com:Shabix/ptolemaios-sdk-package.git',
+    author='Shabix Lampard',
+    author_email='charmzshab@gmail.com',
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
+)q
